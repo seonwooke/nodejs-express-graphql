d8dd0a032e50377f5176aad22d7225eac9967641
seonwooke <seonookkim@gmail.com>
2023-05-18 16:17:41 +0900

    :zap: Apollo
    Apollo

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
 const { loadFilesSync } = require('@graphql-tools/load-files');
 const path = require('path');
-
-const app = express();
 const port = 4000;
 
-const loadFiles = loadFilesSync('**/*', {
-  extensions: ['graphql'], 
-});
+const { ApolloServer } = require('@apollo/server');
+const cors = require('cors');
+const { json } = require('body-parser');
+const { expressMiddleware } = require('@apollo/server/express4');
 
-const loadResolvers = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
+const loadedFiles = loadFilesSync("**/*", {
+    extensions: ['graphql']
+})
 
-const schema = makeExecutableSchema({
-  typeDefs: loadFiles,
-  resolvers: loadResolvers
-});
+const loadedResolvers = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
-const root = {
-  posts: require('./posts/posts.model'),
-  comments: require('./comments/comments.model')
+async function startApolloServer() {
+    const app = express();
+
+    const schema = makeExecutableSchema({
+        typeDefs: loadedFiles,
+        resolvers: loadedResolvers
+    })
+
+    const server = new ApolloServer({
+        schema
+    })
+
+    await server.start();
+
+
+    app.use('/graphql',
+        cors(),
+        json(),
+        expressMiddleware(server, {
+            context: async ({ req }) => ({ token: req.headers.token })
+        }));
+
+    app.listen(port, () => {
+        console.log(`Running a GraphQL API server...`);
+    })
 }
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}));
-
-app.listen(port, () => {
-  console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`);
-});
+startApolloServer();
